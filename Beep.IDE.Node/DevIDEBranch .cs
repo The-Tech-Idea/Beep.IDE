2aa36dd8a98dac0eab646030402a3fa0700304a7
fahad@thetechidea.com <fahad@thetechidea.com>
2024-04-03 23:15:18 +0300

    update

--- Beep.IDE.Node/DevIDEBranch .cs
+++ Beep.IDE.Node/DevIDEBranch .cs
@@ -47,7 +47,7 @@ namespace Beep.IDE.Nodes
         public int ParentBranchID { get; set; }
         public string BranchDescription { get; set; }
         public string BranchClass { get; set; } = "DEV";
-        public object ParentBranch { get; set; }
+         public IBranch ParentBranch { get  ; set  ; }
         public string ObjectType { get; set; } = "Beep.DEV.IDE";
         public IBranch CreateCategoryNode(CategoryFolder p)
         {
